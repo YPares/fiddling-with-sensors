0f23d8301f252c1a9bc4bcadde09203cc3fec5b5
YPares <yves.pares@gmail.com>
2015-05-27 15:38:56 +0200

    Added Makefile from arduino-mk

--- LCD_temp/LCD_temp.ino
+++ LCD_temp/LCD_temp.ino
@@ -1,33 +1,9 @@
-/*
-  fade.ino
-  2013 Copyright (c) Seeed Technology Inc.  All right reserved.
-
-  Author:Loovee
-  2013-10-15
-
-  Grove - Serial LCD RGB Backlight demo.
-
-  This library is free software; you can redistribute it and/or
-  modify it under the terms of the GNU Lesser General Public
-  License as published by the Free Software Foundation; either
-  version 2.1 of the License, or (at your option) any later version.
-
-  This library is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-  Lesser General Public License for more details.
-
-  You should have received a copy of the GNU Lesser General Public
-  License along with this library; if not, write to the Free Software
-  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-*/
-
 #define THERMISTOR_B_VALUE 3975
 #define DHT_PIN A0
 
 #include <Wire.h>
-#include "rgb_lcd.h"
-#include "DHT.h"
+#include <rgb_lcd.h>
+#include <DHT.h>
 
 rgb_lcd lcd;
 DHT dht(DHT_PIN, DHT11);
