94fcff792fb54f64faae8ba10fab669298861b10
YPares <yves.pares@gmail.com>
2016-04-10 18:51:24 +0200

    Server: changed presentation

--- TempHumidServerPy/main.py
+++ TempHumidServerPy/main.py
@@ -5,13 +5,17 @@ import grovepi as g
 
 sensor_pin = 7
 
+
 @route('/')
 def index():
-    [temp, humid] = g.dht(sensor_pin, 0)
-    return template("<h1>Le capteur à Gégé</h1><br/><b>Le capteur dit :\
+    try:
+        [temp, humid] = g.dht(sensor_pin, 0)
+        return template("<h1>Le capteur à Gégé</h1><br/><b>Le capteur dit :\
                      <br/>Température : {{temp}}°C\
                      <br/>Humidité : {{humid}}%", temp=temp, humid=humid)
-
+    except Exception, e:
+        return template("<h1>Oups, rechargez la page</h1>")
+    
 @route('/hello/<name>')
 def index(name):
     return template('<b>Hello {{name}}</b>!', name=name)
